cd5b9311a08f550df69be0aac7e470aea9773363
Benjamin Hernandez <benjamin.hernandez@in2teck.com>
2011-12-07 17:15:06 -0600

    First LunchRabbit Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LunchRabbit::Application.config.secret_token = '303d47ae58fc5c782c84a47ab5b8d24db63a82e3bcb2f85ce81d3b712b6a7e2f38fc163012be56a159a39b1708ae04932fe76509582e5aa2e89f250a760fde3f'
