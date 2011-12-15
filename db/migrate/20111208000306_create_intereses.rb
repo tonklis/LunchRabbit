4317a59f0ea1b5e021908b31fe794392468f7111
Benjamin Hernandez <benjamin.hernandez@in2teck.com>
2011-12-15 16:45:51 -0600

    Se agregaron migraciones para aceptar categoria y id de intereses

--- db/migrate/20111208000306_create_intereses.rb
+++ db/migrate/20111208000306_create_intereses.rb
@@ -2,7 +2,8 @@ class CreateIntereses < ActiveRecord::Migration
   def self.up
     create_table :intereses do |t|
       t.string :nombre
-
+      t.integer :facebook_id, :limit => 8
+      t.string :categoria
       t.timestamps
     end
   end
