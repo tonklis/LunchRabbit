f0b1840a9b5b74b3600caa9082ee4bfb188296db
Benjamin Hernandez <benjamin.hernandez@in2teck.com>
2011-12-19 16:10:08 -0600

    Se arreglaron los scripts de migracion para usuarios

--- db/migrate/20111216215202_add_email_to_usuarios.rb
+++
@@ -1,9 +0,0 @@
-class AddEmailToUsuarios < ActiveRecord::Migration
-  def self.up
-    add_column :usuarios, :email, :string
-  end
-
-  def self.down
-    remove_column :usuarios, :email 
-  end
-end
