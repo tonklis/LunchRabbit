805a6ef0d72143cd0982b0e869c57892a503a2e5
Benjamin Hernandez <benjamin.hernandez@in2teck.com>
2012-01-02 21:36:41 -0600

    Se prueba la logica de sesiones

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,20 +8,3 @@
     //var HOST = "http://localhost:3000"
     document.domain = "heroku.com"
     //document.domain = "localhost";
-
-    window.fbAsyncInit = function() {
-      FB.init({appId: APP_ID, status: true, cookie: true, xfbml: true,  oauth: true});
-      FB.getLoginStatus(function(response) {
-		if (response.status == 'connected') {
-                  //top.location = HOST + $("#app_paths")[0].getAttribute("data-oauth-path");
-		}
-	  });
-	  FB.Event.subscribe('auth.login', function(response) {
-            top.location = HOST + $("#app_paths")[0].getAttribute("data-oauth-path");
-          });
-      FB.Event.subscribe('auth.logout', function(response) {
-        // falta poner logout
-        top.location = HOST;
-      });
-    };    
-
