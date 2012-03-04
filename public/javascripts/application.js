1e9ab12e63c32635deece974c8b1de830cc07ea5
Benjamin Hernandez <benjamin.hernandez@in2teck.com>
2012-03-04 13:46:08 -0600

    Se agrego test para version en produccion

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,9 +2,9 @@
 // This file is automatically included by javascript_include_tag :defaults
 
     // VERSION_PROD
-    // var APP_ID = "316932864999658";
-    var APP_ID = "185077511587462";
-    // var HOST = "http://glowing-moon-5161.heroku.com"
-    var HOST = "http://localhost:3000"
-    // document.domain = "heroku.com"
-    document.domain = "localhost";
+    var APP_ID = "316932864999658";
+    // var APP_ID = "185077511587462";
+    var HOST = "http://glowing-moon-5161.heroku.com"
+    // var HOST = "http://localhost:3000"
+    document.domain = "heroku.com"
+    // document.domain = "localhost";
